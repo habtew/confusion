ca4b465e9bdc310e1442433fdae50f523f649923
Habtamu <habtamuwolde92@gmail.com>
2022-08-30 09:41:28 +0530

    configuring react

---
+++ src/Component.js
@@ -0,0 +1,10 @@
+
+
+export default function Comp(){
+    return(
+     <div className='habtamu'><a 
+     className="App-link"
+     target="_blank"
+     href="https://google.com">Hello mr habtamu</a></div>   
+    )
+}
